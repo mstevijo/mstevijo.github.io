fd0cd80040fce8a4177377bcc16fc9e1d68c33bd
Stevi Jo Malinowski <mstevijo@yahoo.com>
2021-01-06 20:28:16 -0800

    Updates

---
+++ precache-manifest.5cdbfe76524c755b6e6d8c4e275116f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7967ce6b05a512c5e7fb6ae816df156",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f54b2e38c8963acc3f5a",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f54b2e38c8963acc3f5a",
+    "url": "/static/js/main.8582946a.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
